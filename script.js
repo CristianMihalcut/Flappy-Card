50ba3b0e82bd7867f0513d06ae8e0eb7ec4f924a
CristianMihalcut <40146355+CristianMihalcut@users.noreply.github.com>
2025-11-22 11:29:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -92,6 +92,7 @@ class Game {
     init() {
         this.resize();
         window.addEventListener('resize', this.resize);
+        window.addEventListener('orientationchange', () => setTimeout(this.resize, 100));
 
         // Input handling
         window.addEventListener('keydown', (e) => {
